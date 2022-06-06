c35357fb1f72204559433946b7a7130dc3bab4ca
Starfire2014 <106936054+Starfire2014@users.noreply.github.com>
2022-06-05 20:57:51 -0400

    Add files via upload
    First output assignment

---
+++ P1HW1_Output_JacksonBriana.py
@@ -0,0 +1,9 @@
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num*user_num)
+print('And', user_num, 'cubed is', user_num*user_num*user_num, '!!')
+user_num2 = int(input())
+print('Enter another integer:')
+print('4 +',user_num2, 'is', user_num2+4)
+print('4 *',user_num2, 'is', user_num2*4)
